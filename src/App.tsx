a4b9d6429dd9ca457e6e0a519447a6b5a3985fd6
abr-ya <gamer05@list.ru>
2020-12-29 10:09:12 +0300

    Add sound to handler

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import Octave from './components/Octave';
 
 const App = () => {
   const noteHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    // eslint-disable-next-line no-console
     console.log(e.currentTarget.value);
+    const audio = new Audio(`sounds/piano_${e.currentTarget.value}.mp3`);
+    audio.play();
   };
 
   return (
